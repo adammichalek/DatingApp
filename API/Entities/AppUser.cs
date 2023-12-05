88c98055cff261315393fe06807b1a68f4806f2c
adammichalek <43831694+adammichalek@users.noreply.github.com>
2023-12-05 23:34:21 +0100

    Register DTO addition

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,7 +1,4 @@
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Threading.Tasks.Dataflow;
-using Microsoft.AspNetCore.Routing.Tree;
 
 namespace API.Entities
 {
@@ -9,6 +6,8 @@ namespace API.Entities
     public class AppUser
     {
         public int Id { get; set; }
+        
+        [Required]
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
